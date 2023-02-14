c017a655aebf5f0e79f9e3e28f647b851de31065
Holbok Peter <holbok.peter@students.jedlik.eu>
2023-02-14 10:10:30 +0100

    még nincs kész

--- Megoldások.py
+++ Megoldások.py
@@ -1,8 +1,12 @@
+from Szavazatok import Szavazatok
+
+
 class Megoldások:
-    # BCCCDBBBBCDAAA
-    szavazok: int
-    valaszok: int
+    _szavazatok: list[Szavazatok] = []
 
 
-    def __init__(self, sor: str):
-        szavazok, valaszok = sor.split()
+    def __init__(self, fájl_neve: str):
+        self._szavazatok = []
+        with open(fájl_neve, 'r', encoding='utf-8') as file:
+            for sor in file.read().splitlines():
+                self._szavazatok.append(Szavazatok(sor))
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from Megoldások import Megoldások
 
-
 def main() -> None:
-    valaszok: list[Megoldások] = []
-    with open('valaszok.txt', 'r', encoding='utf-8') as file:
-        for sor in file.read().splitlines()[1:]:
-            valaszok.append(Megoldások(sor))
+    # valaszok: list[Szavazatok] = []
+    # with open('valaszok.txt', 'r', encoding='utf-8') as file:
+    #     for sor in file.read().splitlines()[1:]:
+    #         valaszok.append(Szavazatok(sor))
 
 
 if __name__ == "__main__":
