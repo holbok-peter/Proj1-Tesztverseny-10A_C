891e3cd0f9cfd765e3fc453e25be9b2ee7ac3a28
kulacs-kristof <kulacs.kristof@students.jedlik.eu>
2023-02-15 09:27:57 +0100

    2.feladat

--- Megoldasok.py
+++ Megoldasok.py
@@ -4,8 +4,12 @@ from Szavazatok import Szavazatok
 class Megoldasok:
     _szavazatok: list[Szavazatok] = []
 
+    @property
+    def versenyzok_szama(self) -> int:
+        return len(self._szavazatok)
+
     def __init__(self, fájl_neve: str):
         self._szavazatok = []
         with open(fájl_neve, 'r', encoding='utf-8') as file:
-            for sor in file.read().splitlines():
+            for sor in file.read().splitlines()[1:]:
                 self._szavazatok.append(Szavazatok(sor))
