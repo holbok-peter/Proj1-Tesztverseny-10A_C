609ab0666ee3a0f3651a9a71a952f1a7d68cd2a3
kulacs-kristof <kulacs.kristof@students.jedlik.eu>
2023-02-21 09:21:29 +0100

    4.feladat

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main() -> None:
     m = Megoldasok('valaszok.txt')
     print(f'2. feladat: A vetélkedőn {m.versenyzok_szama} versenyző indult.')
 
+#  print(f'4. feladat: {} (a helyes megoldás)')
+
 
 if __name__ == "__main__":
     main()
